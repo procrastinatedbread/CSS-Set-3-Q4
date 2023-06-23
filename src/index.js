745d92ae8d74d12d8facebe3d8ff3ee1f3029fa3
Nikhil Chhabra <91406971+procrastinatedbread@users.noreply.github.com>
2023-06-24 01:56:49 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+
+<div>
+<h1>Ducky</h1>
+<img src="https://picsum.photos/150/150" alt="tasveer"/>
+  
+  <p>We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel </p>
+
+  <button>View</button>
+  
+  
+</div>
+`;
